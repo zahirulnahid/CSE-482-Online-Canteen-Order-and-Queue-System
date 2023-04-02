da730ce54ee09c7d8a89dfc7ed9dfb710d70cb4c
AymanIHakim <aymanibne@hotmail.com>
2023-04-02 22:23:34 +0600

    Update manageAccount.php

--- output/manageAccount.php
+++ output/manageAccount.php
@@ -8,6 +8,7 @@
     <link rel="icon" href="../images/favicon.ico" type="image/x-icon">
     <title>Manage Account</title>
     <link rel="stylesheet" href="outputstyles.css">
+</head>
 
 
 <body class="bg-pink-100 scroll-smooth font-semibold min-h-screen bg-cover bg-no-repeat w-full"
@@ -17,15 +18,18 @@
     <nav class="bg-pink-700 bg-opacity-40 py-4 px-14">
         <div class="container mx-auto flex font-serif justify-between items-center px-4">
             <a href="#" class="text-gray-800 text-2xl font-bold">NSU Canteen - Manage Account</a>
-          
+
             <div>
-            <button class="bg-pink-700 hover:bg-pink-50 hover:text-black text-white font-bold py-3 px-5 rounded-full focus:outline-black 
+                <button
+                    class="bg-pink-700 hover:bg-pink-50 hover:text-black text-white font-bold py-3 px-5 rounded-full focus:outline-black 
                                             focus:ring-2 focus:ring-pink-400 w-full hover:translate-0 hover:transition-shadow"
-                type="submit">Log Out</button>
+                    type="submit">Log Out</button>
             </div>
-            </div>
-        
+        </div>
+
     </nav>
+
+
 
     <div class="flex m-14">
         <!-- <h1 class="text-4xl mx-auto  text-center mb-8 ">Our Menu</h1> -->
@@ -35,46 +39,74 @@
             <input
                 class="border-2 border-gray-400 p-2 text-black rounded-lg focus:outline-none focus:ring-2 focus:ring-pink-400"
                 type="search" name="search" id="search">
-           
+
         </div>
-    
-    
+
+
     </div>
 
     <!-- menu list view -->
     <ul class="grid grid-cols-1 gap-4 mx-auto my-28 container shadow-none">
-        <li class="bg-pink-50 rounded-xl shadow-lg mb-4 overflow-hidden flex">
-            <img src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" alt="Product" class="w-24 h-24 rounded-2xl m-5 object-cover flex-shrink-0">
-            <div class="p-5 flex-grow">
-                <h3 class="text-base font-bold text-gray-900 mb-1">Zahirul Islam Nahid</h3>
-                <p class="text-gray-700 font-medium">2013421</p>
-                <?php
+        <?php
+        include('connection.php');
 
-                if (isset($_POST['edit'])) {
-                    edit();
-                }
-                if (isset($_POST['delete'])) {
-                    delete();
-                }
-                function edit()
-                {
-                    echo "This is edit that is selected";
-                }
-                function delete()
-                {
-                    echo "This is delete that is selected";
-                }
+        // if (isset($_POST['edit'])) {
+        //     edit();
+        // }
+        // if (isset($_POST['delete'])) {
+        //     delete();
+        // }
+        // function edit()
+        // {
+        //     echo "This is edit that is selected";
+        // }
+        // function delete()
+        // {
+        //     echo "This is delete that is selected";
+        // }
+
+        $sql = "SELECT * FROM `users`;";
+        $result = $conn->query($sql);
+
+        //declare array to store the data of database
+        $row = [];
+        if ($result->num_rows > 0) {
+            // fetch all data from db into array 
+            $row = $result->fetch_all(MYSQLI_ASSOC);
+        }
+        if (!empty($row))
+            foreach ($row as $rows) {
                 ?>
-                <form method="post">
-                    <input type="submit" name="delete" value="Delete Account" class=" px-5 py-2 min-w-fit  float-right bg-pink-700 text-gray-100 hover:text-gray-800 hover:bg-pink-100 rounded-full border-spacing-2
+                <li class="bg-pink-50 rounded-xl shadow-lg mb-4 overflow-hidden flex">
+                    <img src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+                        alt="Product" class="w-24 h-24 rounded-2xl m-5 object-cover flex-shrink-0">
+                    <div class="p-5 flex-grow">
+                        <h3 class="text-base font-bold text-gray-900 mb-1">Name:
+                            <?php echo $rows["Name"]; ?>
+                        </h3>
+                        <p class="text-gray-700 font-medium">Email:
+                            <?php echo $rows["Email"]; ?>
+                        </p>
+                        <p class="text-gray-700 font-medium">Category:
+                            <?php echo $rows["Category"]; ?>
+                        </p>
+
+
+                        <form method="post">
+                            <a href="deleteAccount.php?id=<?php echo $rows["Email"]; ?>" name="delete" value="Delete Account" class=" px-5 py-2 min-w-fit  float-right bg-pink-700 text-gray-100 hover:text-gray-800 hover:bg-pink-100 rounded-full border-spacing-2
+                    font-bold focus:ring-2 hover:translate-0 hover:transition-shadow">Delete</a>
+                            <input type="submit" name="edit" value="Edit" class=" px-5 py-2 min-w-fit  float-right bg-pink-700 text-gray-100 hover:text-gray-800 hover:bg-pink-100 rounded-full border-spacing-2
                     font-bold focus:ring-2 hover:translate-0 hover:transition-shadow">
-                    <input type="submit" name="edit" value="Edit" class=" px-5 py-2 min-w-fit  float-right bg-pink-700 text-gray-100 hover:text-gray-800 hover:bg-pink-100 rounded-full border-spacing-2
-                    font-bold focus:ring-2 hover:translate-0 hover:transition-shadow">
-                </form>
-            </div>
-        </li>
-      
+                        </form>
+                        
+                    </div>
+                </li>
+
+                <?php }?>
     </ul>
+
+
 </body>
-</head>
+
+
 </html>
