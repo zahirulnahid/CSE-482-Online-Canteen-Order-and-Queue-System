6b02a5bccf9a3997a8d3ca072ecc28c4b9a3f673
AymanIHakim <aymanibne@hotmail.com>
2023-04-04 02:01:20 +0600

    -

--- output/manageAccount.php
+++ output/manageAccount.php
@@ -8,6 +8,7 @@
     <link rel="icon" href="../images/favicon.ico" type="image/x-icon">
     <title>Manage Account</title>
     <link rel="stylesheet" href="outputstyles.css">
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
 
@@ -64,7 +65,7 @@
         // {
         //     echo "This is delete that is selected";
         // }
-
+        
         $sql = "SELECT * FROM `users`;";
         $result = $conn->query($sql);
 
@@ -87,24 +88,23 @@
                         <p class="text-gray-700 font-medium">Email:
                             <?php echo $rows["Email"]; ?>
                         </p>
+                        <p class="text-gray-700 font-medium">Phone:
+                            <?php echo $rows["Phone"]; ?>
+                        </p>
                         <p class="text-gray-700 font-medium">Category:
                             <?php echo $rows["Category"]; ?>
                         </p>
 
 
-                        <form method="post">
-                            <a href="deleteAccount.php?id=<?php echo $rows["Email"]; ?>" name="delete" value="Delete Account" class=" px-5 py-2 min-w-fit  float-right bg-pink-700 text-gray-100 hover:text-gray-800 hover:bg-pink-100 rounded-full border-spacing-2
+                        <a href="deleteAccount.php?id=<?php echo $rows["Email"]; ?>" name="delete" value="Delete Account" class=" px-5 py-2 min-w-fit  float-right bg-pink-700 text-gray-100 hover:text-gray-800 hover:bg-pink-100 rounded-full border-spacing-2
                     font-bold focus:ring-2 hover:translate-0 hover:transition-shadow">Delete</a>
-                            <input type="submit" name="edit" value="Edit" class=" px-5 py-2 min-w-fit  float-right bg-pink-700 text-gray-100 hover:text-gray-800 hover:bg-pink-100 rounded-full border-spacing-2
-                    font-bold focus:ring-2 hover:translate-0 hover:transition-shadow">
-                        </form>
-                        
+
                     </div>
+
                 </li>
 
-                <?php }?>
+            <?php } ?>
     </ul>
-
 
 </body>
 
