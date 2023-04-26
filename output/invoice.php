7c42eb59b3a446a9fa7452fa025f561a78b20b11
Nafis Faiyaz <56973454+NafisFaiyaz007@users.noreply.github.com>
2023-04-26 14:21:23 +0600

    Update invoice.php

--- output/invoice.php
+++ output/invoice.php
@@ -35,7 +35,7 @@
     </nav>
 
 
-    <div class="bg-gray-200 shadow-lg rounded-lg bg-opacity-40 py-4 mx-auto my-28 px-14 h-full w-1/2">
+    <div class="bg-gray-300 shadow-xl rounded-lg bg-opacity-50 py-4 mx-auto my-28 px-14 h-full w-1/2">
        
         <div class="flex flex-col ">
             <div class="text-2xl text-gray-900 font-raleway mb-4">Cart</div>
@@ -56,13 +56,17 @@
                             $total += $row["Price"]* $row["quantity"];
                 ?>
                 <li class="flex justify-between items-center py-2 px-4 border-b border-gray-500">
-                    <span class="text-gray-800 font-medium"><?php echo $row["Item_Name"];?></span>
-                    <span class="text-pink-500 font-medium"><?php echo $row["Price"];?> BDT</span>
+                    <span class="text-gray-800 font-raleway"><?php echo $row["Item_Name"];?></span>
+                    <span class="text-pink-700 font-raleway"><?php echo $row["Price"];?> BDT</span>
                     <div class="flex item mt-0.5">
-                    <button onclick="updateCart('<?php echo $row['foodID'];?>', 'remove')" class="bg-pink-700 hover:bg-pink-50 hover:text-black text-white font-raleway py-1 px-3 rounded-full focus:outline-black
+                    <button onclick="updateCart('<?php echo $row['foodID'];?>', 'remove')" class="bg-pink-700 hover:bg-pink-50 hover:text-black
+                     text-white font-raleway max-w-fit mr-1 py-1 px-2 rounded-md focus:outline-black
                                 focus:ring-2 focus:ring-pink-400 w-full hover:translate-0 hover:transition-shadow mt-4">-</button>
-                    <span id="quantity-<?php echo $row['foodID'];?>" class="text-pink-500 font-medium"><?php echo $row["quantity"];?> </span>
-                    <button onclick="updateCart('<?php echo $row['foodID'];?>', 'add')" class="bg-pink-700 hover:bg-pink-50 hover:text-black text-white font-raleway py-1 px-3 rounded-full focus:outline-black 
+
+                    <span id="quantity-<?php echo $row['foodID'];?>" class="text-pink-700 font-bold"><?php echo $row["quantity"];?> </span>
+
+                    <button onclick="updateCart('<?php echo $row['foodID'];?>', 'add')" class="bg-pink-700 hover:bg-pink-50 hover:text-black
+                     text-white font-raleway max-w-fit ml-1 py-1 px-2 rounded-md focus:outline-black 
                     focus:ring-2 focus:ring-pink-400 w-full hover:translate-0 hover:transition-shadow mt-4">+</button>
                     </div>
                 </li>
@@ -90,13 +94,14 @@
                 <span class="text-gray-800 font-raleway text-xl">Total:</span>
                 <span class="text-pink-500 font-raleway text-xl"><?php echo $total;?> BDT</span>
             </div>
-            <div class="mx=auto m-5">
-                   <a href="../output/homepage.php" class="bg-gray-100 hover:bg-pink-700 hover:text-white text-black font-raleway mx-auto py-3 px-5 m-4 rounded-full border-pink-600 focus:outline-black 
+            <div class=" flex mx=auto m-5">
+                   <button  class="bg-gray-100 hover:bg-pink-700 hover:text-white text-black font-raleway mx-auto py-3 px-5 m-4 rounded-full border-pink-600 focus:outline-black 
                                             focus:ring-2 focus:ring-pink-400 max-w-fit  hover:translate-0 hover:transition-shadow"
-                type="submit">Back to menu</a>
-            <a href="payment.php?price=<?php echo $total?>" class="bg-pink-700 hover:bg-gray-100 hover:text-black text-white font-raleway mx-auto py-3 px-5 ml-4 rounded-full  focus:outline-4 focus:outline-pink-700 
+                type="submit"><a href="../output/homepage.php" >  Back to menu </a></button>
+
+            <button class="bg-pink-700 hover:bg-gray-100 hover:text-black text-white font-raleway mx-auto  py-3 px-5 m-4 rounded-full  focus:outline-4 focus:outline-pink-700 
                                             focus:ring-2 focus:ring-pink-400 max-w-sm  hover:translate-0 hover:transition-shadow"
-                type="submit">Proceed to Payment</a>
+                type="submit"><a href="payment.php?price=<?php echo $total?>" >Proceed to Payment </a></button>
             </div>
           
 
