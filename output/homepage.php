799d02d126c69a1375de3136dc83a50567d87f8e
Nafis Faiyaz <56973454+NafisFaiyaz007@users.noreply.github.com>
2023-05-08 22:51:06 +0600

    footer update

--- output/homepage.php
+++ output/homepage.php
@@ -96,7 +96,7 @@ include('protection.php'); ?>
         </a>
 
         <!-- my order button -->
-        <a href="invoice.php"
+        <a href="#"
           class="p-4 mt-4 text-center bg-gray-100 text-gray-900 hover:text-gray-100 hover:bg-pink-700 rounded-full font-raleway
           ring-pink-700 ring-2 hover:ring-2 hover:ring-pink-100 hover:translate-0 hover:transition-shadow flex">My Orders</a>
       </div>
@@ -183,41 +183,31 @@ include('protection.php'); ?>
 
   </div>
   </div>
-  <footer class="bg-pink-600 text-white px-4 py-6">
-  <div class="max-w-7xl mx-auto">
-    <div class="flex flex-wrap justify-between">
-      <div class="w-full md:w-1/3 lg:w-1/4 mb-6">
-        <h3 class="text-lg font-medium mb-2">🚹 About Us:</h3>
-        <p class="text-sm">📝 For the purpose of the CSE 482 & CSE 482 <br> Lab project, this website was developed <br> by us. <br><br> Thank you! 🤍</p>
-      </div>
-      <div class="w-full md:w-1/3 lg:w-1/4 mb-6">
-        <h3 class="text-lg font-medium mb-2">🧑🏻‍💻 Our Services:</h3>
-        <ul class="text-sm">
-          <li><a href="#">✅ Universal Canteen Meal Ordering Portal</a></li>
-          <li><a href="#">✅ Automate Canteen Meal Ordering</a></li>
-          <li><a href="#">✅ Introduce Digital Payment</a></li>
-          <li><a href="#">✅ Queue System to Save Time</a></li>
-        </ul>
-      </div>
-      <div class="w-full md:w-1/3 lg:w-1/4 mb-6">
-        <h3 class="text-lg font-medium mb-2">📩 Contact Us:</h3>
-        <p class="text-sm">🌎 Bashundhara R\A; Dhaka, Bangladesh<br>📠 +880-2-55668200<br>📧 Mail Address - 482@nsu.com <br>🕐 Office Hours: Varsity Time </p>
-      </div>
-      <div class="w-full md:w-1/3 lg:w-1/4 mb-6">
-        <h3 class="text-lg font-medium mb-2">🌐 Location:</h3>
-        <div class="flex">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3650.098092052636!2d90.42298167501689!3d23.81511067862635!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c64c103a8093%3A0xd660a4f50365294a!2sNorth%20South%20University!5e0!3m2!1sen!2sbd!4v1683553954593!5m2!1sen!2sbd" width="300" height="150" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+ <footer class="bg-pink-900 text-white font-raleway py-10 ">
+  <div class="container mx-auto px-26">
+    <div class="flex flex-wrap justify-center">
+      <div class="w-full md:w-1/2 lg:w-1/4 mb-8">
+        <h3 class="text-xl font-bold mb-4">Contact Us</h3>
+        <p class="text-sm">Bashundhara R/A, Dhaka, Bangladesh<br>+880-2-55668200<br>482@nsu.com</p>
+        <div class="flex mt-4">
+          <a href="#" class="mr-3 text-gray-400 hover:text-white"><i class="fab fa-facebook-f"></i>facebook</a>
+          <a href="#" class="mr-3 text-gray-400 hover:text-white"><i class="fab fa-twitter"></i>Twitter</a>
+          <a href="#" class="mr-3 text-gray-400 hover:text-white"><i class="fab fa-instagram"></i>Instagram</a>
+          <a href="#" class="text-gray-400 hover:text-white"><i class="fab fa-linkedin-in"></i>Linked-In</a>
         </div>
       </div>
+     <div class="w-full md:w-1/2 lg:w-1/4 mb-8">
+        <h3 class="text-xl font-bold mb-4">Location</h3>
+        <div class="aspect-w-3 aspect-h-2">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3650.098092052636!2d90.42298167501689!3d23.81511067862635!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c64c103a8093%3A0xd660a4f50365294a!2sNorth%20South%20University!5e0!3m2!1sen!2sbd!4v1683553954593!5m2!1sen!2sbd" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+      </div>
+
     </div>
+              <p class="text-center">&copy; 2023 CSE-482™️ All Rights Reserved</p>
+
   </div>
-  <div class="mt-6 border-t border-gray-700 pt-4 flex justify-between items-center">
-    <p class="text-sm">&copy; 2023 CSE™️   All Rights Reserved ✔️</p>
-    <ul class="flex text-sm">
-      <li class="mr-4"><a href="#">📑 Terms of Service</a></li>
-      <li class="mr-4"><a href="#">🔏 Privacy Policy</a></li>
-    </ul>
-  </div>
+  
 </footer>
   <?php $conn->close(); ?>
 </body>
